c6788f6c7baa30746a4cbe329a7aabadea6c1b8f
jacksonpereiram3 <jackson.pereira@mangue3.com>
2018-05-16 10:40:40 -0300

    test

--- public/js/main.js
+++ public/js/main.js
@@ -331,7 +331,7 @@ $(document).ready(function(){
         if (ifTouchDevices){
             $('body').addClass('scroll-touch');
             
-            if(right_side_form.hasClass('hidden') || right_side_clients.hasClass('hidden')){
+            if(!right_side_form.hasClass('hidden') || !right_side_clients.hasClass('hidden')){
                 $('a#open-more-info').on( "click", function() {
                     event.preventDefault();
                     var target = "#" + this.getAttribute('data-target');
@@ -347,7 +347,6 @@ $(document).ready(function(){
                     }, 700, 'swing');
                 });
             }
-            
         }
         
         else {
