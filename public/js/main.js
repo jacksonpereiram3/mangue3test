1add2c2be461f65ffaebb1ac74b5fdf805bf1913
jacksonpereiram3 <jackson.pereira@mangue3.com>
2018-05-16 10:34:59 -0300

    test

--- public/js/main.js
+++ public/js/main.js
@@ -331,7 +331,7 @@ $(document).ready(function(){
         if (ifTouchDevices){
             $('body').addClass('scroll-touch');
             
-            if(!right_side_clients && !right_side_form){
+            if(right_side_form.hasClass('hidden') || right_side_clients.hasClass('hidden')){
                 $('a#open-more-info').on( "click", function() {
                     event.preventDefault();
                     var target = "#" + this.getAttribute('data-target');
